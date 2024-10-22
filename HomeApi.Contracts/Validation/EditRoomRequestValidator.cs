899fea8237f8f75e7250fe1b4e8b871d54d42d90
Филиппов Алексей <sickleshaped@gmail.com>
2024-10-22 13:43:22 +0300

    Добавьте файлы проекта.

---
+++ HomeApi.Contracts/Validation/EditRoomRequestValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using HomeApi.Contracts.Models.Rooms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeApi.Contracts.Validation
+{
+    public class EditRoomRequestValidator : AbstractValidator<EditRoomRequest>
+    {
+        /// <summary>
+        /// Метод, конструктор, устанавливающий правила
+        /// </summary>
+        public EditRoomRequestValidator()
+        {
+            RuleFor(x => x.Area).NotEmpty();
+            RuleFor(x => x.Name).NotEmpty();
+            RuleFor(x => x.Voltage).NotEmpty();
+            RuleFor(x => x.GasConnected).NotEmpty();
+        }
+
+        /// <summary>
+        ///  Метод кастомной валидации для свойства location
+        /// </summary>
+        private bool BeSupported(string location)
+        {
+            return Values.ValidRooms.Any(e => e == location);
+        }
+    }
+}
---
+++ HomeApi.Data/Repos/RoomRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using HomeApi.Data.Models;
+using HomeApi.Data.Queries;
+using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace HomeApi.Data.Repos
+{
+    /// <summary>
+    /// Репозиторий для операций с объектами типа "Room" в базе
+    /// </summary>
+    public class RoomRepository : IRoomRepository
+    {
+        private readonly HomeApiContext _context;
+        
+        public RoomRepository (HomeApiContext context)
+        {
+            _context = context;
+        }
+        
+        /// <summary>
+        ///  Найти комнату по имени
+        /// </summary>
+        public async Task<Room> GetRoomByName(string name)
+        {
+            return await _context.Rooms.Where(r => r.Name == name).FirstOrDefaultAsync();
+        }
+        
+        /// <summary>
+        ///  Добавить новую комнату
+        /// </summary>
+        public async Task AddRoom(Room room)
+        {
+            var entry = _context.Entry(room);
+            if (entry.State == EntityState.Detached)
+                await _context.Rooms.AddAsync(room);
+            
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Room> GetRoomById(Guid id)
+        {
+            return await _context.Rooms.Where(r => r.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task UpdateRoom(Room room, UpdateRoomQuery updateRoomQuery)
+        {
+            if(!string.IsNullOrEmpty(updateRoomQuery.NewName))
+                room.Name = updateRoomQuery.NewName;
+            if (!string.IsNullOrEmpty(updateRoomQuery.NewName))
+                room.Name = updateRoomQuery.NewName;
+            if (!string.IsNullOrEmpty(updateRoomQuery.NewName))
+                room.Name = updateRoomQuery.NewName;
+            if (!string.IsNullOrEmpty(updateRoomQuery.NewName))
+                room.Name = updateRoomQuery.NewName;
+
+            // Добавляем в базу 
+            var entry = _context.Entry(room);
+            if (entry.State == EntityState.Detached)
+                _context.Rooms.Update(room);
+
+            // Сохраняем изменения в базе 
+            await _context.SaveChangesAsync();
+        }
+    }
+}
